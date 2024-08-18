f1422b9d97bf773a790673f3fa01201faf8a2b51
sandali <pldsandali@gmail.com>
2024-08-18 22:47:46 +0530

    express basic structure

--- Server/controllers/customerdetails.tsx
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-var router = express.Router();
-
-router.post('/customerDetails', (req, res) => {
-    console.log(req.body);
-    res.send("Customer details saved");
-})
-
-export default router;
--- Server/index.ts
+++ Server/index.ts
@@ -1,8 +1,16 @@
-import express from "express";
+import express from 'express';
+import MailRoutes from './routers/mailroutes'
+import cors from 'cors';
+
 const app = express();
+const router = express.Router();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.use(cors({ origin: '*' })); // Enable CORS for all origins
+app.use(express.json()); // Middleware to parse JSON
+
+
+app.use('/mail', MailRoutes);
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
 });
-
-app.listen(5000, () => {console.log('Server is running on port 5000')});

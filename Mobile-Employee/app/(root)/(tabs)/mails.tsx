0798ea87971acdbc79f92da6eff98c8dadb29b77
Lishan Dissanayake <dissanayake.lishan@gmail.com>
2024-08-20 08:30:01 +0530

    App structure

---
+++ Mobile-Employee/app/(root)/(tabs)/mails.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Mails = () => {
+  return (
+    <SafeAreaView>
+      <Text>Mails</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Mails;
---
+++ Mobile-Employee/app/(root)/(tabs)/scan.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Scan = () => {
+  return (
+    <SafeAreaView>
+      <Text>Scan</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Scan;

a4b36d1c47bb00695095da3e6223625bf3a37763
sandali <pldsandali@gmail.com>
2024-08-31 13:22:05 +0530

    completed filtered search

--- Server/services/addressservice.ts
+++ Server/services/addressservice.ts
@@ -5,7 +5,7 @@ class AddressService{
     async searchSuggestions(search: string):Promise<string[]> {
         const res = await addressRepostiory.queryAddresses(search);
         const concString = res.map((address) => {
-            return `${address.addressNo}, ${address.streetName}, ${address.postalCode}`
+            return `${address.addressNo? `${address.addressNo}, `: ""  } ${address.streetName? `${address.streetName}, `: ""  } ${address.Locality? `${address.Locality}, `: ""  } ${address.postalCode}`
         })
         console.log(concString);
         return(concString)

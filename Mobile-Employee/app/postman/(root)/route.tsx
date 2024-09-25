9c0ad6f6d45084679cfb99814a1a20608a35217a
Lishan Dissanayake <dissanayake.lishan@gmail.com>
2024-09-25 20:36:30 +0530

    added map

--- Mobile-Employee/app/postman/(root)/route.tsx
+++ Mobile-Employee/app/postman/(root)/route.tsx
@@ -1,8 +1,24 @@
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import MapView from "react-native-maps";
+import { StyleSheet, View } from "react-native";
 
 const Route = () => {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <MapView style={styles.map} />
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: -31,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default Route;

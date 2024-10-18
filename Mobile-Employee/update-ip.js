4a1fa218100b2b7495ad58465b27f3c1e5ec5753
Lishan Dissanayake <dissanayake.lishan@gmail.com>
2024-10-18 21:13:41 +0530

    IP update automated

---
+++ Mobile-Employee/update-ip.js
@@ -0,0 +1,36 @@
+const os = require("os");
+const fs = require("fs");
+const path = require("path");
+
+function getLocalIpAddress() {
+  const networkInterfaces = os.networkInterfaces();
+  for (const interfaceName in networkInterfaces) {
+    const interfaces = networkInterfaces[interfaceName];
+    for (const iface of interfaces) {
+      if (iface.family === "IPv4" && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return null;
+}
+
+function updateConfigIp(newIp) {
+  const configPath = path.join(__dirname, "config.ts");
+  const configContent = fs.readFileSync(configPath, "utf8");
+
+  const updatedContent = configContent.replace(
+    /export const IP = ".*";/,
+    `export const IP = "${newIp}";`
+  );
+
+  fs.writeFileSync(configPath, updatedContent, "utf8");
+  console.log(`Updated IP address in config.ts to: ${newIp}`);
+}
+
+const localIp = getLocalIpAddress();
+if (localIp) {
+  updateConfigIp(localIp);
+} else {
+  console.log("Could not find local IP address.");
+}

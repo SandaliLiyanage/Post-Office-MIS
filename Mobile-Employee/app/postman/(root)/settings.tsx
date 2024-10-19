6528877d7614953c2e5ec8e276a3f3568efd4bc0
Lishan Dissanayake <dissanayake.lishan@gmail.com>
2024-10-20 03:02:49 +0530

    log out

--- Mobile-Employee/app/postman/(root)/settings.tsx
+++ Mobile-Employee/app/postman/(root)/settings.tsx
@@ -12,11 +12,29 @@ import {
   Entypo,
   Ionicons,
 } from "@expo/vector-icons";
+import { useUser } from "../../auth/usercontext";
+import { useNavigation } from "@react-navigation/native";
+import { ROUTES } from "./routes";
+import { useRouter } from "expo-router";
 
 const Settings = () => {
+  const router = useRouter();
+  const { removeUser, user } = useUser();
+  const navigation = useNavigation();
+  type RouteKeys = keyof typeof ROUTES;
+
+  const handleNavigation = (routeName: RouteKeys) => {
+    router.push(ROUTES[routeName] as any); // Navigate to the selected route
+  };
+
+  const handleLogout = () => {
+    removeUser(); // This removes the user from the context or clears the session
+    handleNavigation("LOG_IN"); // Navigate to the login screen
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity style={styles.settingItem}>
+      {/* <TouchableOpacity style={styles.settingItem}>
         <View style={styles.iconContainer}>
           <FontAwesome5 name="user-alt" size={22} color="#fff" />
         </View>
@@ -28,9 +46,9 @@ const Settings = () => {
           <MaterialIcons name="security" size={25} color="#fff" />
         </View>
         <Text style={styles.settingText}>Change Password</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
 
-      <TouchableOpacity style={styles.settingItem}>
+      <TouchableOpacity style={styles.settingItem} onPress={handleLogout}>
         <View style={styles.iconContainer}>
           <MaterialIcons name="logout" size={25} color="#fff" />
         </View>
@@ -43,21 +61,15 @@ const Settings = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#1c1c1e", // Dark background color
+    backgroundColor: "#F9001E",
     padding: 20,
-  },
-  header: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "#fff",
-    marginBottom: 30,
   },
   settingItem: {
     flexDirection: "row",
     alignItems: "center",
     paddingVertical: 15,
     borderBottomWidth: 1,
-    borderBottomColor: "#333", // Slightly lighter color for separators
+    borderBottomColor: "#CD0000", // Slightly lighter color for separators
   },
   iconContainer: {
     width: 40, // Fixed width for consistent icon alignment
@@ -66,16 +78,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: "#fff", // White text color
     marginLeft: 10,
-  },
-  logoutButton: {
-    borderTopWidth: 1,
-    borderTopColor: "#333", // To match the styling of the other items
-    marginTop: 20,
-    paddingTop: 15,
-    backgroundColor: "#ff4c4c", // Distinct red color for the log-out button
-  },
-  logoutButtonText: {
-    fontWeight: "bold",
   },
 });
 

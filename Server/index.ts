330e741d7a72c480e5fb1593551e93264e8a6cbe
sandali <pldsandali@gmail.com>
2024-10-22 21:09:12 +0530

    Refactor IP configuration and package dependencies

--- Server/index.ts
+++ Server/index.ts
@@ -33,7 +33,7 @@ app.use("/address", AddressRoutes);
 app.use("/money-order",  MoneyOrderRoutes);
 
 
-const port = 5001;
+const port = 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port} `);
 });

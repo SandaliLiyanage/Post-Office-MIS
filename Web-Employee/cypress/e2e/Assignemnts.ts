108f43564c82306729660d885e89b19b29398f50
sandali <pldsandali@gmail.com>
2024-10-23 15:44:00 +0530

    Refactor e2e tests for receptionist login and assignment page

--- Web-Employee/cypress/e2e/Assignemnts.ts
+++
@@ -1,7 +0,0 @@
-describe('login for receptionist', () => {
-    it('existence', () => {
-        cy.visit('/dashboard/postmanAssignments');
-        cy.get('[data-testId="cypress-postmanAssignments-title"]', { timeout: 10000 }).should("exist")
-        .should('have.text', 'Mail and Area Assignments');
-      })
-    })

5c38f4ba1a1dacd3fa299ca39ba48dbcb4a561dc
Lishan Dissanayake <dissanayake.lishan@gmail.com>
2024-10-23 17:39:26 +0530

    IP

--- Mobile-Employee/config.ts
+++ Mobile-Employee/config.ts
@@ -1,3 +1,3 @@
-//export const IP = "152.42.182.120";
+export const IP = "152.42.182.120";
 //export const IP = "192.168.228.26";
-export const IP = "192.168.1.64";
+//export const IP = "192.168.1.64";

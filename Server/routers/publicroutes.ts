4b51d195b1a282d3929e671fc250a037c5973bad
sandali <pldsandali@gmail.com>
2024-10-25 20:16:14 +0530

    public routes

--- Server/routers/publicroutes.ts
+++ Server/routers/publicroutes.ts
@@ -3,9 +3,12 @@ import {
   } from "../controllers/mailcontroller";
 import { Router } from "express";
 import { getTrackingDetails } from "../controllers/mailcontroller";
+import { estimateDeliveryTime } from "../controllers/mailcontroller";
 
 const router = Router();
 console.log("in public routes")
 router.post("/calculatePrice", CalculatePrice);
 router.post("/track", getTrackingDetails);
+router.post("/estimate-delivery-time", estimateDeliveryTime);
+
 export default router;
